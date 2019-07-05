aa96d82663de4f13211535103657679e02a7d5af
Aditi Joshi <adijoshi2@publicisgroupe.net>
2019-07-05 16:13:56 +0530

    week1

--- Assessment/src/com/bawarchi/comparators/PriceLTComparator.java
+++ Assessment/src/com/bawarchi/comparators/PriceLTComparator.java
@@ -4,12 +4,10 @@ import java.util.Comparator;
 
 import com.bawarchi.model.Dish;
 
-public class PriceLTComparator implements Comparator<Dish>{
-
+public class PriceLTComparator implements Comparator<Dish> {
 	@Override
 	public int compare(Dish d1, Dish d2) {
 		// TODO Auto-generated method stub
 		return Double.valueOf(d1.getPrice()).compareTo(d2.getPrice());
 	}
-
 }
--- Assessment/src/com/bawarchi/model/Dish.java
+++ Assessment/src/com/bawarchi/model/Dish.java
@@ -2,13 +2,13 @@ package com.bawarchi.model;
 
 import java.util.Objects;
 
-public class Dish implements Comparable<Dish>{
-	private static int counter=1;
-	private int id ;
-    private String name;
-    private double calories;
-    private double price ;
-    
+public class Dish implements Comparable<Dish> {
+	private static int counter = 1;
+	private int id;
+	private String name;
+	private double calories;
+	private double price;
+
 	public Dish(int id, String name, double price, double calories) {
 		this.id = id;
 		this.name = name;
@@ -47,7 +47,7 @@ public class Dish implements Comparable<Dish>{
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	@Override
 	public int compareTo(Dish o) {
 		return Integer.valueOf(this.id).compareTo(o.id);
@@ -55,15 +55,13 @@ public class Dish implements Comparable<Dish>{
 
 	@Override
 	public String toString() {
-		return ("Dish id: "+this.id + "Name: "+ this.name +" Price: " + this.price + " calories: " +
-	      this.calories);
+		return ("Dish id: " + this.id + "Name: " + this.name + " Price: " + this.price + " calories: " + this.calories);
 	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(this.id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -73,26 +71,22 @@ public class Dish implements Comparable<Dish>{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Dish other = (Dish) obj;
-		if (id != other.id)
+		Dish other_dish = (Dish) obj;
+		if (id != other_dish.id)
 			return false;
 		return true;
 	}
-	
+
 	public void payment(double amount) {
-		if(amount>this.price)
-		{
+		if (amount > this.price) {
 			double change = amount - this.price;
-			System.out.println("========Thanks for your order. Please collect the change=Rs"+change+ "================");
-		}
-		else if(amount == this.price)
-		{
+			System.out.println(
+					"========Thanks for your order. Please collect the change=Rs" + change + "================");
+		} else if (amount == this.price) {
 			System.out.println("========Thanks for your order================");
-		}
-		else
-		{
+		} else {
 			System.out.println("========Amount paid is less than bill================");
 		}
 	}
-	
+
 }
